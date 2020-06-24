aaffa3798e4ff89ffd8d5a6a358e21bdd8da210e
bala-arun-s <balaarun.sankaran@gmail.com>
2020-06-24 21:11:17 +0530

    login done

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class LoginPage implements OnInit {
   user: any={};
 
-  constructor(private router: Router,public http:HttpClient) { }
+  constructor(private router: Router,public http:HttpClient,public alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -18,9 +19,24 @@ export class LoginPage implements OnInit {
     console.log(this.user,"Client: I am sending data to server for login");
       this.http.post("http://localhost:3000/login",this.user,
       {headers:new HttpHeaders({"Content-Type":"application/json"})}).subscribe((response) => {
-        console.log(response);
+        console.log(response)
+        if (response === null){
+          this.showAlert('Log in failed!','Invalid username or password');
+        }else if (response === "false user") {
+          this.showAlert('Log in failed!','Invalid username or password');
+        }else if (response === 'true user') {
+          this.router.navigate(['/home']);
+        };
       })
-    //this.router.navigate(['/home']);
+  }
+
+  async showAlert(h,m) {
+    const alert = await this.alertController.create({
+      header: h,
+      message: m,
+    });
+
+    await alert.present();
   }
 
   SignUp(){
